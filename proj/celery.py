056af08c95bd03da5511a8aa84fc4d93bd70e518
Zhanghao Wu <zhanghao.wu@outlook.com>
2022-08-06 19:04:58 -0700

    Add exampel for nonblocking inference

--- proj/celery.py
+++ proj/celery.py
@@ -7,13 +7,35 @@ import sky
 
 app = Celery(__name__, broker='redis://localhost:6379/0', result_backend='redis://localhost:6379/0')
 
-SETUP_CMD = 'conda env list'
+SETUP_CMD = """\
+    conda activate myenv
+    if [ $? -eq 0 ]; then
+        echo "conda env exists"
+    else
+        echo "creating conda env"
+        conda create -n myenv python=3.8 -y
+        conda activate myenv
+        pip install tqdm
+    fi
+    """
 
-RUN_CMD = 'echo My hostname: $(hostname)'
+RUN_CMD = """\
+    echo start training
+    conda activate myenv
+    python -u - <<EOF
+        import time
+        import tqdm
+        print('Start training for image {image_id}')
+        # for i in tqdm.tqdm(range(120)):
+        for i in tqdm.tqdm(range(10)):
+            # 2 minutes training
+            time.sleep(1)
+    EOF
+    """
 
 
 @app.task
-def run_sky(cluster_name: str) -> str:
+def run_sky(cluster_name: str, image_id) -> str:
     print(f'Started with cluster_name: {cluster_name}')
     with sky.Dag() as dag:
         # The run command will be run on *all* nodes.
@@ -22,7 +44,7 @@ def run_sky(cluster_name: str) -> str:
         #   My hostname: <host2>
         sky.Task(
             setup=SETUP_CMD,
-            run=RUN_CMD,
+            run=RUN_CMD.format(image_id=image_id),
             num_nodes=1).set_resources(sky.Resources(sky.AWS()))
 
     sky.launch(dag, cluster_name=cluster_name)
--- run.py
+++ run.py
@@ -1,11 +1,43 @@
+import threading
+import time
 from proj.celery import run_sky
 
-sky_task = run_sky.s('test-celery-sky-1')
+infer_tasks = []
+finished = False
 
-print('Starting')
-future = sky_task.apply_async()
+def inference_image(image_id: str):
+    sky_task = run_sky.s('test-celery-sky', image_id)
 
-print('Waiting for result')
-future.get()
+    print(f'Starting Task for image {image_id}')
+    infer_task = sky_task.apply_async()
+    infer_tasks.append((image_id, infer_task))
+    print(f'Task for image {image_id} started.')
 
-print('Finished')
+def wait_for_tasks():
+    while not finished or len(infer_tasks) > 0:
+        if len(infer_tasks) == 0:
+            time.sleep(1)
+            continue
+
+        for item in infer_tasks:
+            image_id, infer_task = item
+            if infer_task.ready():
+                print(f'\nTask for image {image_id} finished.')
+                infer_tasks.remove(item)
+            
+
+if __name__ == '__main__':
+    task_thread = threading.Thread(target=wait_for_tasks)
+    task_thread.start()
+
+    while True:
+        image_id = input('Enter image id: ')
+        if image_id == 'q':
+            finished = True
+            break
+        inference_image(image_id)
+
+    task_thread.join()
+
+
+
